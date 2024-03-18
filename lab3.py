7f3c12329e8558e34aa27f400d78d1966420886e
Zlata <zlata@172.23.64.164.wi-fi.pjwstk.edu.pl.pjwstk.edu.pl>
2024-03-18 12:26:33 +0100

    added implementation of task1 to lab3.py

---
+++ lab3.py
@@ -0,0 +1,5 @@
+# task 1. Write a Python program that generates a list of squares of numbers from 1 to 10 using list comprehensions.ares)
+squares = [i**2 for i in range(1,11)]
+
+# task 2. Expand the previous program by defining a function that takes a range of numbers as input and returns a list of 
+# squares for that range. e_squares(start,end))
