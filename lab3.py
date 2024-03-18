ebf9c81b96a25ce29228b0e6bbea5393206572cd
Zlata <zlata@172.23.64.164.wi-fi.pjwstk.edu.pl.pjwstk.edu.pl>
2024-03-18 12:41:05 +0100

    fixed tasks 2-3, added solution to task 4
    fixed arguments in functions of tasks 2-3

--- lab3.py
+++ lab3.py
@@ -1,12 +1,20 @@
+import math
+
 # task 1. Write a Python program that generates a list of squares of numbers from 1 to 10 using list comprehensions.ares)
 squares = [i**2 for i in range(1,11)]
+print(squares)
 
 # task 2. Expand the previous program by defining a function that takes a range of numbers as input and returns a list of 
 # squares for that range. e_squares(start,end))
-def squares_of_range(input_range):
-    range_squares = [i**2 for i in input_range]
+def squares_of_range(start, end):
+    range_squares = [i**2 for i in range(start, end+1)]
 
 # task 3. Create a class called SquareGenerator that has a method to generate squares for a given range of numbers.
 class SquareGenerator:
-    def squares_of_range(input_range):
-        generate_squares = [i**2 for i in input_range]
+    def squares_of_range(start, end):
+        generate_squares = [i**2 for i in range(start, end+1)]
+
+# task 4. Utilize the math library to calculate the squareroot of each number in the generated list from the previous task.
+roots = [math.sqrt(i) for i in squares]
+print(roots)
+
